4227f12c60ea387bdde1be7e95bda88b58b3754b
icozamiz <iozamiz>
2021-03-04 21:03:47 -0500

    fix donate page

--- src/pages/donate/donate-data.ts
+++ src/pages/donate/donate-data.ts
@@ -3,10 +3,10 @@ export const donateEn = {
   description:
     "Canadian Women’s Foundation focuses on the most critical challenges facing women and girls, and invests in the most effective solutions for long-term change. The Foundation funds programs in the following issue areas:",
   infoPoints: [
-    '<a href="https://canadianwomen.org/out-of-violence/" target="_blank">Out of Violence:</a> Like you, we want all women and girls to live free of violence.',
-    '<a href="https://canadianwomen.org/out-of-poverty/" target="_blank">Out of Poverty:</a> Like you, we want women living on low incomes to have opportunities to move themselves out of poverty.',
-    '<a href="https://canadianwomen.org/empower-girls/" target="_blank">Empower Girls:</a> Like you, we want all girls to believe in themselves and realize they matter.',
-    '<a href="https://canadianwomen.org/inclusive-leadership/" target="_blank">Inclusive Leadership:</a> Like you, we want all women and girls to realize their potential for leadership.',
+    '<a href="https://canadianwomen.org/out-of-violence/" target="_blank">Out of Violence:</a> Like you, they want all women and girls to live free of violence.',
+    '<a href="https://canadianwomen.org/out-of-poverty/" target="_blank">Out of Poverty:</a> Like you, they want women living on low incomes to have opportunities to move themselves out of poverty.',
+    '<a href="https://canadianwomen.org/empower-girls/" target="_blank">Empower Girls:</a> Like you, they want all girls to believe in themselves and realize they matter.',
+    '<a href="https://canadianwomen.org/inclusive-leadership/" target="_blank">Inclusive Leadership:</a> Like you, they want all women and girls to realize their potential for leadership.',
   ],
   buttonText: "Donate",
   title2: "Fund this project",
